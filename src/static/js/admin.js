dba758d2b3ca7a009e01d64a394f26fbeff99f02
Michail Pautov <pautovmichail@gmail.com>
2023-05-28 12:09:23 +0300

    Update numbers mask

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    $('.ac-mask-int').mask('A', {translation: {'A': {pattern: /[0-9]/, recursive: true}}});
-    $('.ac-mask-float').mask('A', {translation: {'A': {pattern: /[0-9.,]/, recursive: true}}});
+    $('.ac-mask-int').mask('A', {translation: {'A': {pattern: /[0-9-]/, recursive: true}}});
+    $('.ac-mask-float').mask('A', {translation: {'A': {pattern: /[0-9.,-]/, recursive: true}}});
     $('.ac-mask-email').mask('A', {translation: {'A': {pattern: /[@a-zA-Z0-9_.-]/, recursive: true}}, placeholder: 'example@domain.ru'});
     $('.ac-mask-phone').mask('0 (000) 000-00-00', {placeholder: '- (---) --- -- --'});
     $('.ac-mask-hex').mask('ABBBBBB', {translation: {'A': {pattern: /[#]/}, 'B': {pattern: /[a-zA-Z0-9]/}}, placeholder: '#000000'});
